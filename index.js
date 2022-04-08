c920405c1c0a32ba1e4145075fb18f8cdd40d84c
Alex McDaniel <alexpmcdaniel@gmail.com>
2022-04-07 21:21:16 -0400

    commit tree acbfe6cdb0bb3c84b750adfdd66aaee849bbb040

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+main.remove()
+
+let newHeader = document.createElement('h1')
+newHeader.textContent='YOUR-NAME is the champion'
+newHeader.id='victory'
+console.log(newHeader)
